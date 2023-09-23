e861fed569f4c999fd9b5563ddf4c3c00fe6dac1
韭皇啦 <51317376+zyancn@users.noreply.github.com>
2023-09-23 05:37:03 -0500

    Update get_up.go

--- github/get_up.go
+++ github/get_up.go
@@ -36,7 +36,7 @@ func makeGetupMsg(city string) (body string, isGetupEarly bool) {
 	now := time.Now().In(cstZone)
 	// 3点到18点起床都有效
 	isGetupEarly = 3 < now.Hour() && now.Hour() < 24
-	body = fmt.Sprintf("今天的起床时间是--%s\n", now.Format(time.Kitchen))
+	body = fmt.Sprintf("今天的起床时间是--%s\n上班赚本金！继续梭哈crypto！", now.Format(time.Kitchen))
 
 	if len(weatherMsg) > 0 {
 		body = fmt.Sprintf("%s\n %s\n", body, weatherMsg)
